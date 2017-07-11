f9f58c69a1c10de4d7b27497938471b516a2a46a
edwinSe <esbp1998@gmail.com>
2017-07-11 16:38:36 +0200

    Modificacion Tareas

--- src/main/java/com/netmind/resources/TareaResource.java
+++ src/main/java/com/netmind/resources/TareaResource.java
@@ -12,39 +12,47 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.netmind.db.ProyectoDAO;
+import com.netmind.db.ProyectoDAOImpl;
+import com.netmind.db.TareaDAO;
+import com.netmind.db.TareaDAOImpl;
 import com.netmind.models.Message;
 import com.netmind.models.User;
 import com.netmind.models.proyectos;
 import com.netmind.models.tareas;
 
 @Path("/tareas")
-public class TareasResource {
+public class TareaResource {
 	private static List<tareas> tareasUser;
 	private static tareas UnaTarea;
-	
+
 	/* GET|POST /Lista de tareas del Usuario solicitado */
-	@Path("/usuario/{uid}")
+	@Path("/")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<tareas> getTareasListUser(@PathParam("uid") int uid) {		
-		
+	public List<tareas> getTareasListUser(@PathParam("pid") int pid) {
+
 		// acceso a la clase devolver TAREAS del usuario
-		
-		
+
+		TareaDAO tarPers = (TareaDAO) TareaDAOImpl.getInstance();
+		this.tareasUser = tarPers.getTareas(pid);
+
 		return this.tareasUser;
-	}	
-	
-	
+	}
+
 	/* POST Insertar un tarea */
 	@Path("/")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message insertTarea(tareas nuevoOeditTarea) {
-				
-		// añadiriamos un proyecto si este id de TAREA no existe lo creamos si existe lo modificamos
-		
-		
+
+		// añadiriamos un proyecto si este id de TAREA no existe lo creamos si
+		// existe lo modificamos
+
+		TareaDAO tarPers = (TareaDAO) TareaDAOImpl.getInstance();
+		this.tareasUser.add(nuevoOeditTarea);
+
 		return new Message("Tarea Añadida");
 	}
 
@@ -54,36 +62,53 @@ public class TareasResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public tareas getTareaUser(@PathParam("tid") int tid) {
 
-	   /*	User unUser = new User();
-		for (User user : misUsuarios) {
-			if(user.getUid()==uid){
-				unUser=user;
+		TareaDAO tarPers = (TareaDAO) TareaDAOImpl.getInstance();
+		tareas unaTarea = new tareas(tid, tid, tid, null, null);
+		for (tareas tarea : tareasUser) {
+			if (tarea.getTid() == tid) {
+				unaTarea = tarea;
 				break;
 			}
 		}
-		*/
-		
+
 		// acceso a la Bdatos para devolver una tarea.
 
 		return this.UnaTarea;
 	}
-	
+
+	@Path("/{tid}")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateTarea(@PathParam("tid") int tid, tareas unaTarea) {
+
+		TareaDAO tarPers = (TareaDAO) TareaDAOImpl.getInstance();
+		for (tareas aTarea : tareasUser) {
+			if (aTarea.getUid() == tid) {
+				tareasUser.remove(aTarea);
+				tareasUser.add(unaTarea);
+				break;
+			}
+		}
+
+		return new Message("Tareas modificadas");
+	}
+
 	@Path("/{tid}")
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message deleteTarea(@PathParam("tid") int tid) {
-		
-		/*
-		for (User user : misUsuarios) {
-			if(user.getUid()==uid){
-				misUsuarios.remove(user);
+
+		TareaDAO tarPers = (TareaDAO) TareaDAOImpl.getInstance();
+		for (tareas tarea : tareasUser) {
+			if (tarea.getTid() == tid) {
+				tareasUser.remove(tarea);
 				break;
 			}
 		}
-		*/
 
 		return new Message("Tarea Borrado");
-	}	
-	
+	}
+
 }
